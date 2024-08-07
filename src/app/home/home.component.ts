0a1cf3c687bf723cd5c1edb06cbd16cbb44a8773
dijialei <dijialei0605@gmail.com>
2024-08-08 00:05:09 +0200

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,NavComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
